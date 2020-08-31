edcb6965913d8ff947f4e046175d5fcc302ab539
Alicja <alicja.waclawek@gmail.com>
2020-08-31 21:46:06 +0200

    added categories

---
+++ src/main/java/com/alicjawaclawek/memy/Category.java
@@ -0,0 +1,42 @@
+package com.alicjawaclawek.memy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class Category {
+
+    private int id;
+    private String name;
+
+//    public static List<Category> ALL_CATEGORIES = new ArrayList<>();
+//
+//    static(
+//            ALL_CATEGORIES.add(new Category(1,"Osoby"));
+//            ALL_CATEGORIES.add(new Category(2,"Sport"));
+//            ALL_CATEGORIES.add(new Category(3,"Memy"));
+//    )
+    public static final List<Category> ALL_CATEGORIES = Arrays.asList(
+            new Category(1,"Osoby"),
+            new Category(2,"Sport"),
+            new Category(3,"Memy")
+    );
+
+    public List<Category> getAllCategories(){
+        return ALL_CATEGORIES;
+    }
+    public Category findById(int id){
+        for (Category category:ALL_CATEGORIES){
+            if (category.getId()==id){
+                return category;
+            }
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/alicjawaclawek/memy/HomeController.java
+++ src/main/java/com/alicjawaclawek/memy/HomeController.java
@@ -1,6 +1,5 @@
 package com.alicjawaclawek.memy;
 
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,14 @@ public class HomeController {
 
     @GetMapping("/favorites")
     public String getFavorites(ModelMap map) {
-        map.put("gifs", Gif.GIFS);
+        List<Gif> gifList = new ArrayList<>();
+        for (Gif gif : Gif.GIFS
+        ) {
+            if (gif.isFavorite()) {
+                gifList.add(gif);
+            }
+        }
+        map.put("gifs", gifList);
         return "favorites";
     }
 
@@ -32,19 +38,21 @@ public class HomeController {
     String name="mike"
      */
     @GetMapping("/gif/{name}")
-    public String getDetails(@PathVariable String name,ModelMap map) {
+    public String getDetails(@PathVariable String name, ModelMap map) {
 //        for (int i = 0; i < 6; i++) {
 //            if (name.equals(Gif.GIFS.get(i).getName()))
 //                map.put("gif", Gif.GIFS.get(i));
 //        }
 
-        map.put("gif",Gif.GIFS.stream().
+        map.put("gif", Gif.GIFS.stream().
                 filter(g -> g.getName().equals(name)).findFirst().get());
         return "gif-details";
     }
-//@GetMapping("/favorites")
-//    public String getFavorites(){
-//
-//        return "favorites";
-//}
+
+    @GetMapping("/categories")
+    public String showCategories(ModelMap map){
+        map.put("categories",Category.ALL_CATEGORIES);
+        return "categories";
+    }
 }
+
