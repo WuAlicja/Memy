9e70787165a58013191f621f998476e1d2fc7b04
Alicja <alicja.waclawek@gmail.com>
2020-08-31 15:01:29 +0200

    added gif details

--- src/main/java/com/alicjawaclawek/memy/HomeController.java
+++ src/main/java/com/alicjawaclawek/memy/HomeController.java
@@ -1,8 +1,10 @@
 package com.alicjawaclawek.memy;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +15,38 @@ public class HomeController {
     //model map sluzy do przekazywania zmiennej do html
     @GetMapping("/")
     public String getHome(ModelMap map) {
-        map.put("gifs",Gif.GIFS);
+        map.put("gifs", Gif.GIFS);
         return "home";
     }
+
     @GetMapping("/favorites")
     public String getFavorites(ModelMap map) {
-        map.put("gifs",Gif.GIFS);
+        map.put("gifs", Gif.GIFS);
         return "favorites";
     }
 
+    //name ktory jest zmienny
+    /*
+    @PathVariable zamienia automatycznie ze sciezki ktora sie zmienia na zmienna Javowa o nazwie name
+    git/mike
+    String name="mike"
+     */
+    @GetMapping("/gif/{name}")
+    public String getDetails(@PathVariable String name,ModelMap map) {
+//        for (int i = 0; i < 6; i++) {
+//            if (name.equals(Gif.GIFS.get(i).getName()))
+//                map.put("gif", Gif.GIFS.get(i));
+//        }
+
+        map.put("gif",Gif.GIFS.stream().
+                filter(g -> g.getName().equals(name)).findFirst().get());
+        return "gif-details";
+    }
+
+    @GetMapping("/posladek")
+    public String getPosladek(ModelMap map){
+
+        map.put("posadek","Moj super posladek");
+        return "posladek";
+    }
 }
