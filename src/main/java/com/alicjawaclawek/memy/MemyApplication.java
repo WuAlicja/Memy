ee39393f1ed3f4cda0df950ffc913dc303cb7899
Alicja <alicja.waclawek@gmail.com>
2020-08-31 11:04:45 +0200

    initialized and added front and added home controller

---
+++ src/main/java/com/alicjawaclawek/memy/MemyApplication.java
@@ -0,0 +1,13 @@
+package com.alicjawaclawek.memy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemyApplication.class, args);
+	}
+
+}
